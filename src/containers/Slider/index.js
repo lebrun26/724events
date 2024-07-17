6edb027a50ad45f4263d6a94173b99ca53b2d5a1
lebrun26 <lebrun.patrickdev@gmail.com>
2024-07-17 09:05:30 +0200

    correction du checked dans le fichier slider

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -50,6 +50,7 @@ const Slider = () => {
               type="radio"
               name="radio-button"
               checked={index === radioIdx}
+              readOnly
             />
           ))}
         </div>
